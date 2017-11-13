ad6e09243be43262d5d1ebe391a7846204f32514
mattbk <bedrocks@gmail.com>
2017-11-13 04:52:50 +0000

    Use relative path for database connection.
    There's no need to place this in /var/www either, because Flask runs its own server, not Apache.

--- lab_app/lab_app.py
+++ lab_app/lab_app.py
@@ -135,7 +135,7 @@ def get_records():
 		from_date_utc   = arrow.get(from_date_obj, timezone).to('Etc/UTC').strftime("%Y-%m-%d %H:%M")	
 		to_date_utc     = arrow.get(to_date_obj, timezone).to('Etc/UTC').strftime("%Y-%m-%d %H:%M")
 
-	conn 			    = sqlite3.connect('/var/www/lab_app/lab_app.db')
+	conn 			    = sqlite3.connect('lab_app.db')
 	curs 			    = conn.cursor()
 	curs.execute("SELECT * FROM temperatures WHERE rDateTime BETWEEN ? AND ?", (from_date_utc.format('YYYY-MM-DD HH:mm'), to_date_utc.format('YYYY-MM-DD HH:mm')))
 	temperatures 	    = curs.fetchall()
